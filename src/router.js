db31f6eaebe499439db8f2921451b3b520d01108
Nimira43 <n.rabone72@aol.com>
2025-01-09 06:32:19 +0000

    Wrote imports for the pages.

--- src/router.js
+++ src/router.js
@@ -1,5 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import MentorDetail from './pages/mentors/MentorDetail.vue'
+import MentorsList from './pages/mentors/MentorsList.vue'
+import MentorRegistration from './pages/mentors/MentorRegistration.vue'
+import ContactMentor from './pages/requests/ContactMentor.vue'
+import RequestsReceived from './pages/requests/RequestsReceived.vue'
+import NotFound from './pages/NotFound.vue'
+
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
