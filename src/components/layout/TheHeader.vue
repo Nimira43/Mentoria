cb3c02cebd2706ca948eab9cb2ee5fa61be0a90e
Nimira43 <n.rabone72@aol.com>
2025-01-09 10:45:09 +0000

    Setting up basic header navigation

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,7 +1,23 @@
 <template>
   <header>
     <nav>
-      <h1 class="logo">Mentoria</h1>
+      <h1>
+        <router-link to="/">Search Mentors</router-link>
+      </h1>
+      <ul>
+        <li>
+          <router-link to="/mentors">All Mentors</router-link>
+        </li>
+        <li>
+          <router-link to="/requests">Requests</router-link>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
+
+<style scoped>
+  header {
+
+  }
+</style>
