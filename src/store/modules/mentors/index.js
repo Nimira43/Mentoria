190ff168eda4f7d52287ca2ef4621ba199c6207a
Nimira43 <n.rabone72@aol.com>
2025-01-18 11:25:39 +0000

    Brought in some made up Mentors data.

--- src/store/modules/mentors/index.js
+++ src/store/modules/mentors/index.js
@@ -4,7 +4,30 @@ import getters from './getters.js'
 
 export default {
   namespaced: true,
-  state() {},
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Stanley',
+          lastName: 'Kubrick',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm Stanley and I've work in Web Development for the last 10 years. With my experience I'm sure I'll be able to guide you in your career.",
+          hourlyRate: 42
+        },
+        {
+          id: 'c2',
+          firstName: 'Alex',
+          lastName: 'Cox',
+          areas: ['backend', 'career'],
+          description:
+            "I'm  Alex and have worked on large scale backend projects for multi-national companies like Sony and Ford. I'd love the opportunity to help you progress in your field.",
+          hourlyRate: 36
+        }
+      ]
+    }
+  },
   mutations,
   actions,
   getters
