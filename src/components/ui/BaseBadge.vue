b456b51704d4fa96460abe136d173ad71cbc3a29
Nimira43 <n.rabone72@aol.com>
2025-01-23 13:24:10 +0000

    BaseBadge UI component done.

---
+++ src/components/ui/BaseBadge.vue
@@ -0,0 +1,41 @@
+<template>
+  <span class="badge" :class="type">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+  export default {
+    props: ['type', 'title'],
+    computed: {
+      text() {
+        return this.title.toUpperCase()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .badge {
+    background: purple;
+    color: white;
+    border-radius: 5px;
+    padding: 0.5rem 1.5rem;
+    display: inline-block;
+    margin-right: 0.5rem;
+  }
+  .frontend {
+    background-color: green;
+    color: white;
+  }
+  
+  .backend {
+    background-color: darkgreen;
+    color: white;
+  }
+  
+  .career {
+    background-color: violet;
+    color: white;
+  }
+</style>
