6934cd14132aeddd502e6f9b8b7d1cb41d5b1121
Nimira43 <n.rabone72@aol.com>
2025-01-23 11:34:07 +0000

    template, script and styles completed in BaseButton.vue.

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button 
+    v-if="!link" 
+    :class="mode"
+  >
+    <slot></slot>
+  </button>
+  <router-link 
+    v-else :to="to" 
+    :class="mode"
+  >
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+  export default {
+    props: {
+      mode: {
+        type: String,
+        required: false,
+        default: null
+      },
+      link: {
+        type: Boolean,
+        required: false,
+        default: false
+      },
+      to: {
+        type: String,
+        required: false,
+        default: '/'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  button, a {
+    text-decoration: none;
+    padding: 0.75rem 1,5rem;
+    font: inherit;
+    background-color: #111;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+    border-radius: 5px;
+    margin-right: 0.5rem;
+    display: inline-block;
+    transition: all 0.5s ease-in-out;
+  }
+
+  a:hover, a:active,
+  button:hover, button:active {
+    background-color: black;
+  }
+
+  
+</style>
