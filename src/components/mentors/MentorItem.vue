6761a5a9255daf905e06859ac00d782869d666b8
Nimira43 <n.rabone72@aol.com>
2025-01-23 10:04:47 +0000

    Created MentorItem component.

---
+++ src/components/mentors/MentorItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }} / hour</h4>
+    <div>
+      <span 
+        v-for="area in areas" 
+        :key="area"
+      >
+        {{ area }}
+      </span>
+    </div>
+    <div class="actions">
+      <router-link
+        to="/mentors/c1/contact"
+      >
+        Contact
+      </router-link>
+      <router-link
+        to="/mentors/c1"
+      >
+        View Details
+      </router-link>
+    </div>
+  </li>
+</template>
