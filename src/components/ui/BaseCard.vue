cde7a6a66557eda4acf9b932549ec63001996d6d
Nimira43 <n.rabone72@aol.com>
2025-01-23 11:21:09 +0000

    Completed BaseCard.vue template and styles.

---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+  .card {
+    border-radius: 5px;
+    box-shadow: 0 2px 8px(0, 0, 0, 0.3);
+    padding: 1rem;
+    margin: 2rem auto;
+    max-width: 40rem;
+  }
+</style>
