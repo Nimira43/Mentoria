2d023bf0c18e9ad9b2ff8ce861e3dd41471eaee6
Nimira43 <n.rabone72@aol.com>
2025-02-11 12:06:28 +0000

    Setting up markup for Mentor Details

--- src/pages/mentors/MentorDetail.vue
+++ src/pages/mentors/MentorDetail.vue
@@ -1,5 +1,23 @@
 <template>
-  Mentor Details
-  <router-view></router-view>
-  <router-link to="/mentors/user-1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>£{{ rate }} / hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Find to out more please me.</h2>
+        <base-button link :to='contactLink'>Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for='area in areas' :key='area' :type='area' :title='area'></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
