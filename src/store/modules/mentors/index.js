84831623722c722a5b5056d41dbbdeef533fa9fd
Nimira43 <n.rabone72@aol.com>
2025-02-24 10:50:50 +0000

    Added more mentor data.

--- src/store/modules/mentors/index.js
+++ src/store/modules/mentors/index.js
@@ -24,6 +24,24 @@ export default {
           description:
             "I'm  Alex and have worked on large scale backend projects for multi-national companies like Sony and Ford. I'd love the opportunity to help you progress in your field.",
           hourlyRate: 36
+        },
+        {
+          id: 'c3',
+          firstName: 'David',
+          lastName: 'Lynch',
+          areas: ['frontend', 'career'],
+          description:
+            "I'm David, specialising in frontend development and career coaching. Having a deep understanding of career development strategies I'm eager to guide you in advancing your career while mastering frontend technologies.",
+          hourlyRate: 38
+        },
+        {
+          id: 'c4',
+          firstName: 'Stephanie',
+          lastName: 'Lucas',
+          areas: ['frontend', 'backend'],
+          description:
+            "I'm Stephanie.I've worked with cutting-edge technologies and delivered successful projects for global companies. I'm excited to share my expertise and help you achieve your goals in both frontend and backend development.",
+          hourlyRate: 45
         }
       ]
     }
