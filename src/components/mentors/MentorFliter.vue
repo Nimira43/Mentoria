1a43317548014398db0d86c7dd63cf1dadab8a92
Nimira43 <n.rabone72@aol.com>
2025-02-24 09:08:46 +0000

    Added checkbox inputs for selecting mentor specialty

---
+++ src/components/mentors/MentorFliter.vue
@@ -0,0 +1,18 @@
+<template>
+  <base-card>
+    <h2>Search for a Mentor</h2>
+    <span class="filter-option">
+      <input type="checkbox" id="frontend" checked />
+      <label for="frontend">Frontend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="backend" checked />
+      <label for="backend">Backend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="career" checked />
+      <label for="career">Career</label>
+    </span>
+  </base-card>
+
+</template>
