00f3a23a38f01c115d623e638fcef5a80fbf97ab
Nimira43 <n.rabone72@aol.com>
2025-03-31 23:12:44 +0100

    Binding inputs

--- src/components/mentors/MentorForm.vue
+++ src/components/mentors/MentorForm.vue
@@ -2,32 +2,63 @@
   <form>
     <div class="form-control">
       <label for="firstname">Firstname</label>
-      <input type="text" id="firstname" />
+      <input 
+        type="text" 
+        id="firstname" 
+        v-model.trim="firstName"  
+      />
     </div>
     <div class="form-control">  
       <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" />
+      <input 
+        type="text" 
+        id="lastname" 
+        v-model.trim="lastName"  
+      />
     </div>
     <div class="form-control">  
       <label for="description">Description</label>
-      <textarea id="description" rows="5"></textarea>
+      <textarea 
+        id="description" 
+        rows="5"
+        v-model.trim="description"
+      ></textarea>
     </div>
     <div class="form-control">  
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" />
+      <input 
+        type="number" 
+        id="rate" 
+        v-model.number="rate"
+      />
     </div>
     <div class="form-control">
       <h3>Areas of Expertise</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend">
+        <input 
+          type="checkbox" 
+          id="frontend" 
+          value="frontend"
+          v-model="areas"
+        >
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" value="backend">
+        <input 
+          type="checkbox" 
+          id="backend" 
+          value="backend"
+          v-model="areas"
+        >
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career">
+        <input 
+          type="checkbox" 
+          id="career" 
+          value="career"
+          v-model="areas"
+        >
         <label for="career">Career Development</label>
       </div>
     </div>
