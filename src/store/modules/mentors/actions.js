5a9978ef9209d8c0e24b805e436f416d2dac8501
Nimira43 <n.rabone72@aol.com>
2025-05-10 10:52:35 +0100

    registerMentor in action.ts and mutations.js

--- src/store/modules/mentors/actions.js
+++ src/store/modules/mentors/actions.js
@@ -1,1 +1,11 @@
-export default {}
+export default {
+  registerMentor(context, data) {
+    const mentorData = {
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyDate: data.rate,
+      areas: data.areas
+    }
+  }
+}
