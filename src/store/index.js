74f637bdddceecccf1ff334bc5ba16ed16b53688
Nimira43 <n.rabone72@aol.com>
2025-06-23 21:01:33 +0100

    Adding state with userId

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,11 @@ import mentorsModule from './modules/mentors/index.js'
 const store = createStore ({
   modules: {
     mentors: mentorsModule
+  },
+  state() {
+    return {
+      userId: 'c3' // hard-coded for now
+    }
   }
 })
 
